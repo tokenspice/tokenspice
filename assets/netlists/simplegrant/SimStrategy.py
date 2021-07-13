a85cc338923ac76d0337115490578347b5feb64d
trentmc <5305452+trentmc@users.noreply.github.com>
2021-07-13 13:18:54 +0300

    Make SimStrategy simpler

--- assets/netlists/simplegrant/SimStrategy.py
+++ assets/netlists/simplegrant/SimStrategy.py
@@ -1,20 +1,18 @@
-import math
 from enforce_typing import enforce_types
 
 from engine import SimStrategyBase
-from util.constants import S_PER_DAY, S_PER_HOUR
+from util.constants import S_PER_HOUR, S_PER_DAY
 
 @enforce_types
 class SimStrategy(SimStrategyBase.SimStrategyBase):
     def __init__(self):
         super().__init__()
 
-        #===set base-class values we want for this netlist====
+        #==baseline
         self.setTimeStep(S_PER_HOUR)
-        max_days = 10
-        self.setMaxTicks(max_days * S_PER_DAY / self.time_step + 1)
+        self.setMaxTime(10, 'days')
 
-        #===new attributes specific to this netlist===
+        #==attributes specific to this netlist
         self.granter_init_USD: float = 0.0
         self.granter_init_OCEAN: float = 1.0
         self.granter_s_between_grants: int = S_PER_DAY*3
--- assets/netlists/simplepool/SimStrategy.py
+++ assets/netlists/simplepool/SimStrategy.py
@@ -1,24 +1,23 @@
-import math
 from enforce_typing import enforce_types
 
 from engine import SimStrategyBase
-from util.constants import S_PER_DAY, S_PER_HOUR
+from util.constants import S_PER_HOUR
 
 @enforce_types
 class SimStrategy(SimStrategyBase.SimStrategyBase):
     def __init__(self):
         super().__init__()
 
-        #===set base-class values we want for this netlist====
+        #==baseline
         self.setTimeStep(S_PER_HOUR)
-        max_days = 20
-        self.setMaxTicks(max_days * S_PER_DAY / self.time_step + 1)
+        self.setMaxTime(20, 'days')
 
-        #===new attributes specific to this netlist===
-        #when publisher is created
+        #==attributes specific to this netlist
+        
+        #publisher 
         self.pub_init_OCEAN = 10000.0
 
-        #for pool creation
+        #pool
         self.OCEAN_init = 1000.0
         self.OCEAN_stake = 200.0
         self.DT_init = 100.0
