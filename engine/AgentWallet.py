bc80d341e113f3de3d3949b5954228b1f7eae966
nvtrang91 <nvtrang91@gmail.com>
2021-07-28 11:41:41 +0700

    Implement rest of DataConsumerAgent, unskip its tests #19

--- engine/AgentWallet.py
+++ engine/AgentWallet.py
@@ -5,7 +5,7 @@ from enforce_typing import enforce_types
 import typing
 
 from web3engine import bpool, btoken, datatoken, globaltokens
-from util import constants 
+from util import constants
 from util.strutil import asCurrency
 from web3tools import web3util, web3wallet
 from web3tools.web3util import fromBase18, toBase18
@@ -28,7 +28,7 @@ class AgentWallet:
 
         #Give the new wallet ETH to pay gas fees (but don't track otherwise)
         self._web3wallet.fundFromAbove(toBase18(0.01)) #magic number
-        
+
         #USD
         self._USD = USD #lump in ETH too
 
@@ -37,27 +37,27 @@ class AgentWallet:
                                value_base=toBase18(OCEAN))
         self._cached_OCEAN_base: typing.Union[int,None] = None #for speed
 
-        #amount 
+        #amount
         self._total_USD_in:float = USD
         self._total_OCEAN_in:float = OCEAN
 
     def resetCachedInfo(self):
         self._cached_OCEAN_base = None
-        
+
     @property
     def _address(self):
          return self._web3wallet.address
-     
-    #=================================================================== 
-    #USD-related   
+
+    #===================================================================
+    #USD-related
     def USD(self) -> float:
         return self._USD
-        
+
     def depositUSD(self, amt: float) -> None:
         assert amt >= 0.0
         self._USD += amt
         self._total_USD_in += amt
-        
+
     def withdrawUSD(self, amt: float) -> None:
         assert amt >= 0.0
         if amt > 0.0 and self._USD > 0.0:
@@ -79,22 +79,22 @@ class AgentWallet:
     def totalUSDin(self) -> float:
         return self._total_USD_in
 
-    #===================================================================  
-    #OCEAN-related 
+    #===================================================================
+    #OCEAN-related
     def OCEAN(self) -> float:
         return fromBase18(self._OCEAN_base())
 
     def _OCEAN_base(self) -> int:
         if self._cached_OCEAN_base is None:
             self._cached_OCEAN_base = globaltokens.OCEANtoken().balanceOf_base(self._address)
-        return self._cached_OCEAN_base            
-        
+        return self._cached_OCEAN_base
+
     def depositOCEAN(self, amt: float) -> None:
         assert amt >= 0.0
         globaltokens.mintOCEAN(self._address, toBase18(amt))
         self._total_OCEAN_in += amt
         self.resetCachedInfo()
-        
+
     def withdrawOCEAN(self, amt: float) -> None:
         self.transferOCEAN(_BURN_WALLET, amt)
 
@@ -102,12 +102,12 @@ class AgentWallet:
         assert isinstance(dst_wallet, AgentWallet) or \
             isinstance(dst_wallet, BurnWallet)
         dst_address = dst_wallet._address
-        
+
         amt_base = toBase18(amt)
         assert amt_base >= 0
         if amt_base == 0:
             return
-        
+
         OCEAN_base = self._OCEAN_base()
         if OCEAN_base == 0:
             raise ValueError("no funds to transfer from")
@@ -122,14 +122,14 @@ class AgentWallet:
 
         globaltokens.OCEANtoken().transfer(
             dst_address, amt_base, self._web3wallet)
-        
+
         dst_wallet._total_OCEAN_in += amt
         self.resetCachedInfo()
-        dst_wallet.resetCachedInfo()        
+        dst_wallet.resetCachedInfo()
 
     def totalOCEANin(self) -> float:
         return self._total_OCEAN_in
-        
+
     #===================================================================
     #ETH-related. Not much here because we use it little, just for gas
     def ETH(self) -> float:
@@ -137,18 +137,18 @@ class AgentWallet:
 
     def _ETH_base(self) -> int: #i.e. num wei
         return self._web3wallet.ETH_base()
-    
+
     #===================================================================
     #datatoken and pool-related
     def DT(self, dt:datatoken.Datatoken) -> float:
         return fromBase18(self._DT_base(dt))
 
-    def _DT_base(self, dt:datatoken.Datatoken) -> int: 
+    def _DT_base(self, dt:datatoken.Datatoken) -> int:
         return dt.balanceOf_base(self._address)
-    
+
     def BPT(self, pool:bpool.BPool) -> float:
         return fromBase18(self._BPT_base(pool))
-    
+
     def _BPT_base(self, pool:bpool.BPool) -> int:
         return pool.balanceOf_base(self._address)
 
@@ -167,7 +167,24 @@ class AgentWallet:
             from_wallet=self._web3wallet,
         )
         self.resetCachedInfo()
-                
+
+    def buyDT(self, pool:bpool.BPool, DT:datatoken.Datatoken,
+               DT_buy_amt:float, max_OCEAN_allow:float):
+        """Swap OCEAN for DT """
+        OCEAN = globaltokens.OCEANtoken()
+        OCEAN.approve(pool.address, toBase18(max_OCEAN_allow),
+                      from_wallet=self._web3wallet)
+
+        pool.swapExactAmountOut(
+            tokenIn_address=globaltokens.OCEAN_address(),
+            maxAmountIn_base=toBase18(max_OCEAN_allow),
+            tokenOut_address=DT.address,
+            tokenAmountOut_base=toBase18(DT_buy_amt),
+            maxPrice_base=2 ** 255,
+            from_wallet=self._web3wallet,
+        )
+        self.resetCachedInfo()
+
     def stakeOCEAN(self, OCEAN_stake:float, pool:bpool.BPool):
         """Convert some OCEAN to DT, then add both as liquidity."""
         OCEAN = globaltokens.OCEANtoken()
@@ -179,7 +196,7 @@ class AgentWallet:
             minPoolAmountOut_base=toBase18(0.0),
             from_wallet=self._web3wallet)
         self.resetCachedInfo()
-        
+
     def unstakeOCEAN(self, BPT_unstake:float, pool:bpool.BPool):
         pool.exitswapPoolAmountIn(
             tokenOut_address=globaltokens.OCEAN_address(),
@@ -209,7 +226,7 @@ class BurnWallet:
     def __init__(self):
         self._address = constants.BURN_ADDRESS
         self._total_OCEAN_in:float = 0.0
-        
+
     def resetCachedInfo(self):
         pass
 
