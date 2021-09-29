4ee22902300a0849cc0e059f0e3dc022b6eb5da1
JohannSuarez <johann.suarez92@gmail.com>
2021-09-28 17:18:20 -0700

    Added ganache.py --run-in-background

--- ganache.py
+++ ganache.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import eth_utils
+import argparse
 import os
 
 from web3tools.web3util import confFileValue
+
+# Argument parsing for hiding ganache.py in background
+parser = argparse.ArgumentParser()
+parser.add_argument("--run-in-background", action='store_true',
+                    help="Run ganache on background for further use of current terminal.")
+args = parser.parse_args()
 
 # Port in ganache_url must match ganache-cli call
 port_number = '8545'
@@ -21,5 +28,11 @@ factory_deployer_private_key = confFileValue(
 # launch ganache-cli and give each private account 100 eth.
 amount_eth = 100
 amount_wei = eth_utils.to_wei(amount_eth, 'ether')
-os.system(
-    f'ganache-cli --port {port_number} --gasLimit 10000000000 --gasPrice 1 ---hardfork istanbul --account="{alice_private_key},{amount_wei}" --account="{bob_private_key},{amount_wei}" --account="{factory_deployer_private_key},{amount_wei}" &')
+
+ganache_command = f'ganache-cli --port {port_number} --gasLimit 10000000000 --gasPrice 1 ---hardfork istanbul --account="{alice_private_key},{amount_wei}" --account="{bob_private_key},{amount_wei}" --account="{factory_deployer_private_key},{amount_wei}"'
+
+
+if args.run_in_background:
+    os.system(ganache_command + '&')
+else:
+    os.system(ganache_command)
