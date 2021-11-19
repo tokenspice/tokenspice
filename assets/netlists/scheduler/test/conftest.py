beac80d2c19b863a8cad26c7296ce1af5608ef89
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-19 09:47:32 +0100

    now all unit tests pass together

--- assets/netlists/scheduler/test/conftest.py
+++ assets/netlists/scheduler/test/conftest.py
@@ -3,21 +3,21 @@
 import brownie
 import pytest
 
-#https://eth-brownie.readthedocs.io/en/stable/tests-pytest-intro.html#fixture-scope
-@pytest.fixture(scope="module") #this scope means contract is only deployed once
+BROWNIE_PROJECT = brownie.project.load(
+    'assets/netlists/scheduler', name="MyProject")
+brownie.network.connect('development')
+
+@pytest.fixture
+def project():
+    return BROWNIE_PROJECT
+
+@pytest.fixture
 def accounts():
     return brownie.network.accounts
 
-@pytest.fixture(scope="module") 
+@pytest.fixture
 def token():
-    my_project = _load_project()
-    token = my_project.Datatoken.deploy(
+    t = BROWNIE_PROJECT.Datatoken.deploy(
         "TST", "Test Token", "myblob", 18, 1e21,
         {'from' : brownie.network.accounts[0]})
-    return token
-
-def _load_project():
-    my_project = brownie.project.load(
-        'assets/netlists/scheduler', name="MyProject")
-    brownie.network.connect('development')
-    return my_project
+    return t
