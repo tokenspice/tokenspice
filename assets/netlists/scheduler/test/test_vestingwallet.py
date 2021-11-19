92b8759920a7ad3fdb8289696974c46ce758bc6e
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-19 11:14:17 +0100

    testing ETH transfers first, wip here

--- assets/netlists/scheduler/test/test_vestingwallet.py
+++ assets/netlists/scheduler/test/test_vestingwallet.py
@@ -13,20 +13,16 @@ def test_noFunding(vesting_wallet, accounts, chain):
     vesting_wallet.release()
     assert vesting_wallet.released() == 0 #wallet never got funds _to_ release!
 
-def test_tokenFunding(project, accounts, chain):
-    #account0 gets TST token
-    token = project.Datatoken.deploy(
-        "TST", "Test Token", "myblob", 18, Wei('1 ether'),
-        {'from' : accounts[0]})
-    assert token.balanceOf(accounts[0].address) == Wei('1 ether')
+def test_ethFunding(project, accounts, chain):
+    #account 0 should start with 100 ETH
+    assert accounts[0].balance()/1e18 == approx(100.0)
 
-    #account0 can freely send TST token.
-    token.transfer(accounts[1], Wei('0.1 ether'), {'from': accounts[0]})
-    assert token.balanceOf(accounts[0].address) == Wei('0.9 ether')
-    assert token.balanceOf(accounts[1].address) == Wei('0.1 ether')
-
-    #let's lock things up, so that all ETH & tokens that account0 holds are
-    # now subject vesting. This includes its TST tokens.
+    #account0 should be able to freely transfer ETH
+    accounts[0].transfer(accounts[1], "50 ether")
+    assert accounts[0].balance()/1e18 == approx(50.0)
+        
+    #force vesting upon account0. Now, all ETH & tokens that account0 gets
+    # are subject to vesting. 
     beneficiary_address = accounts[1].address
     start_timestamp = chain.time() + 5 #magic number
     duration_seconds = 30 #magic number
@@ -34,10 +30,11 @@ def test_tokenFunding(project, accounts, chain):
         beneficiary_address, start_timestamp, duration_seconds,
         {'from' : accounts[0]})
 
-    #if account0 tries to transfer all its TST, it will fail
-    #account0 can freely send TST token
+    chain.mine(blocks=3, timedelta=100) #more than enough time for all vesting
+    
+    #if account0 tries to transfer all its ETH, it should fail
     import pdb; pdb.set_trace()
-    token.transfer(accounts[1], Wei('0.9 ether'), {'from': accounts[0]})
-    assert token.balanceOf(accounts[0].address) > 0, "sending shoulda been blocked"
+    accounts[0].transfer(accounts[1], "50 ether") #this line should fail
+    
 
 
