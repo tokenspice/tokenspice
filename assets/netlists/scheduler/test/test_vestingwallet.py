7e80a9fb6e0b93280c04a9a1adc824ab89d6f397
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-20 05:19:01 +0100

    fleshed out all tests on sending/receiving ETH. Looking good!

--- assets/netlists/scheduler/test/test_vestingwallet.py
+++ assets/netlists/scheduler/test/test_vestingwallet.py
@@ -18,8 +18,9 @@ def test_ethFunding(project, accounts, chain):
     assert accounts[0].balance()/1e18 == approx(100.0)
 
     #account0 should be able to freely transfer ETH
-    accounts[0].transfer(accounts[1], "50 ether")
-    assert accounts[0].balance()/1e18 == approx(50.0)
+    accounts[0].transfer(accounts[1], "10 ether")
+    assert accounts[0].balance()/1e18 == approx(90.0)
+    assert accounts[1].balance()/1e18 == approx(110.0)
         
     #force vesting upon account0. Now, all ETH & tokens that account0 gets
     # are subject to vesting. 
@@ -30,11 +31,40 @@ def test_ethFunding(project, accounts, chain):
         beneficiary_address, start_timestamp, duration_seconds,
         {'from' : accounts[0]})
 
-    chain.mine(blocks=3, timedelta=100) #more than enough time for all vesting
+    #send money to the wallet. It has a function:
+    #    receive() external payable virtual {}
+    #which allows it to receive ETH. It's called for plain ETH transfers,
+    #ie every call with empty calldata.
+    #https://medium.com/coinmonks/solidity-v0-6-0-is-here-things-you-should-know-7d4ab5bca5f1
+    accounts[0].transfer(wallet.address, "90 ether")
+    assert accounts[0].balance() == 0
+    assert accounts[1].balance()/1e18 == approx(110.0)
+    assert wallet.vestedAmount(chain.time()) == 0
+    assert wallet.released() == 0
+
+    #make enough time pass for everything to vest
+    chain.mine(blocks=3, timedelta=100)
+    assert wallet.vestedAmount(chain.time())/1e18 == approx(90.0)
+    assert wallet.released() == 0
+    assert accounts[1].balance()/1e18 == approx(110.0) #not released yet!
+
+    #release the ETH. Anyone can call it
+    wallet.release({'from': accounts[2]})
+    assert wallet.released()/1e18 == approx(90.0) #now it's released!
+    assert accounts[1].balance()/1e18 == approx(200.0) #beneficiary is richer
+
+    #put some new ETH in. It's already immediately vested, but not released
+    accounts[2].transfer(wallet.address, "10 ether")
+    assert wallet.vestedAmount(chain.time())/1e18 == approx(100.0)
+    assert wallet.released()/1e18 == approx(90.0) #not released yet!
+
+    #release the new ETH
+    wallet.release({'from': accounts[3]})
+    assert wallet.released()/1e18 == approx(100.0) #now new ETH is released!
+    assert accounts[1].balance()/1e18 == approx(210.0) #beneficiary got +10 ETH
     
-    #if account0 tries to transfer all its ETH, it should fail
-    import pdb; pdb.set_trace()
-    accounts[0].transfer(accounts[1], "50 ether") #this line should fail
+    
+    
     
 
 
