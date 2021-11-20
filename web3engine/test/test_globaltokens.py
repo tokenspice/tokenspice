64453684472bf7f3e1eb90a44481b20951270401
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-20 14:22:34 +0100

    reimplement globaltokens.py with brownie. Much simpler

--- web3engine/test/test_globaltokens.py
+++ web3engine/test/test_globaltokens.py
@@ -1,15 +1,5 @@
-from web3engine import globaltokens, datatoken
-from web3tools import web3util, web3wallet
+from web3engine import globaltokens
 
 def test_OCEAN():
-    wallet = web3wallet.randomWeb3Wallet()
-    
-    OCEAN_base = web3util.toBase18(3.0)
-    globaltokens.mintOCEAN(address=wallet.address, value_base=OCEAN_base)
-    OCEAN_token = globaltokens.OCEANtoken()
-    assert isinstance(OCEAN_token, datatoken.Datatoken)
-    assert OCEAN_token.symbol() == 'OCEAN'
-    assert OCEAN_token.balanceOf_base(wallet.address) == OCEAN_base
-    
-    globaltokens.mintOCEAN(address=wallet.address, value_base=OCEAN_base) 
-    assert OCEAN_token.balanceOf_base(wallet.address) == OCEAN_base * 2
+    assert globaltokens.OCEANtoken().symbol() == 'OCEAN'
+    assert globaltokens.OCEAN_address()[:2] == '0x'
