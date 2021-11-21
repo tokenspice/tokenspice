45db8451bf415715cb70ab509b922188ed26097b
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-21 08:24:22 +0100

    remove releaseOCEAN() from here, rather, call the wallet directly to do things. Simpler

--- assets/agents/VestingWalletAgent.py
+++ assets/agents/VestingWalletAgent.py
@@ -11,16 +11,11 @@ class VestingWalletAgent(AgentBase.AgentBaseEvm):
     #this vesting wallet never owns stuff itself, therefore USD=OCEAN=0
     def __init__(self, name: str, vesting_wallet):
         super().__init__(name, USD=0.0, OCEAN=0.0)
-        self._vesting_wallet = vesting_wallet # a brownie Contract
+        self._vesting_wallet = vesting_wallet
 
     @property
-    def vesting_wallet(self): #a brownie Contract
+    def vesting_wallet(self):
         return self._vesting_wallet
-    
-    @property
-    def releaseOCEAN(self):
-        """Release vested OCEAN, send them to beneficiary. Anyone can call."""
-        self._vesting_wallet.release(OCEAN_address())
         
     def takeStep(self, state):
         #it's a smart contract robot, it doesn't initiate anything itself
