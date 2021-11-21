bfa86f58759700aa42182ff54b01b855daaaa75e
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-21 08:25:05 +0100

    test this agent more thoroughly. It's a subset of the tests in contracts/test_VestingWallet

--- assets/agents/test/test_VestingWalletAgent.py
+++ assets/agents/test/test_VestingWalletAgent.py
@@ -1,28 +1,44 @@
 import brownie
+from brownie import Wei
 from enforce_typing import enforce_types
 from pytest import approx
 
 from assets.agents import VestingWalletAgent
 from util.constants import BROWNIE_PROJECT
+from web3engine import globaltokens
+accounts = brownie.network.accounts
+chain = brownie.network.chain
 
-class MockState:
-    pass
-    
 @enforce_types
-def test1(project):
-    #deploy contract
-    beneficiary_address = brownie.network.accounts[1].address
-    start_timestamp = brownie.network.chain.time() + 5 #magic number
-    duration_seconds = 30 #magic number
-    vw_contract = BROWNIE_PROJECT.VestingWallet.deploy(
-        beneficiary_address, start_timestamp, duration_seconds,
-        {'from' : brownie.network.accounts[0]})
-
-    #create agent
-    vw_agent = VestingWalletAgent.VestingWalletAgent("vw_agent", vw_contract)
-    assert vw_agent.vesting_wallet.address == vw_contract.address
+def test1():
+    token = globaltokens.OCEANtoken()
     
-    state = MockState()
-    vw_agent.takeStep(state)
+    start_timestamp = chain.time() + 5
+    duration_seconds = 30
+    vw_orig = BROWNIE_PROJECT.VestingWallet.deploy(
+        accounts[1].address, start_timestamp, duration_seconds,
+        {'from' : accounts[0]})
 
-    #fancier tests are in 'scheduler' netlist
+    vw_agent = VestingWalletAgent.VestingWalletAgent("vw_agent", vw_orig)
+    vw = vw_agent.vesting_wallet
+    assert id(vw) == id(vw_orig)
+    globaltokens.fundOCEANFromAbove(vw.address, Wei("5 ether"))
+    
+    class MockState:
+        def takeStep(self):
+            pass
+    state = MockState()
+    state.takeStep() #nothing happens, because this doesn't advance time
+    
+    #make time pass. **THIS SHOULD PROBABLY GO INTO state.takeStep()!!**
+    chain.mine(blocks=3, timedelta=100)
+    assert vw.vestedAmount(token.address, chain.time()) == Wei("5 ether")
+    assert vw.released() == 0
+    assert token.balanceOf(accounts[1].address) == 0
+
+    #release the OCEAN. Anyone can call it
+    vw.release(token.address, {'from': accounts[2]})
+    assert vw.released(token.address)/1e18 == approx(5.0) #now it's released!
+    assert token.balanceOf(accounts[1].address) == Wei("5 ether") #beneficiary is richer
+
+    
