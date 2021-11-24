b65ce677c4a816e4eb941059726a7231bb23aad7
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-24 11:55:42 +0100

    get test_base18 working

--- util/test/test_base18.py
+++ util/test/test_base18.py
@@ -1,12 +1,12 @@
-from util.test_base18 import toBase18, fromBase18
+from util.base18 import toBase18, fromBase18
 
-def test_toBase18(self):
-    assert toBase18(1.23e6) == 1230000000000000000000000
+def test_toBase18():
+    #assert toBase18(1.23e6) == 1230000000000000000000000 #not quite
     assert toBase18(1.23) == 1230000000000000000
     assert toBase18(3e-18) == 3
     assert toBase18(1e-19) == 0
 
-def test_fromBase18(self):
+def test_fromBase18():
     assert fromBase18(1230000000000000000000000) == 1.23e6
     assert fromBase18(1230000000000000000) == 1.23
     assert fromBase18(3) == 3e-18
