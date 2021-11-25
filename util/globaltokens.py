8b4f8e4d7b13a77710d13ad406b11dd2b1514460
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-25 10:26:28 +0100

    work for when brownie reverts the chain after each unit test. When it reverts, it doesn't re-import and therefore maintains the _OCEAN_TOKEN symbol. This new logic handles that.

--- util/globaltokens.py
+++ util/globaltokens.py
@@ -4,17 +4,24 @@ from enforce_typing import enforce_types
 from util.constants import BROWNIE_PROJECT, GOD_ACCOUNT
 from util.base18 import toBase18
 
-_OCEAN_TOKEN = GOD_ACCOUNT.deploy(
-    BROWNIE_PROJECT.Simpletoken, 'OCEAN', 'OCEAN', 18, toBase18(1e9))
+_OCEAN_TOKEN = None
     
 @enforce_types
-def OCEAN_address() -> str:
-    return _OCEAN_TOKEN.address
+def OCEANtoken():
+    global _OCEAN_TOKEN
+    try:
+        token = _OCEAN_TOKEN
+    except brownie.exceptions.ContractNotFound:
+        token = None
+    if token is None:
+        token = _OCEAN_TOKEN = GOD_ACCOUNT.deploy(
+            BROWNIE_PROJECT.Simpletoken, 'OCEAN', 'OCEAN', 18, toBase18(1e9))
+    return token
 
 @enforce_types
-def OCEANtoken():
-    return _OCEAN_TOKEN
+def OCEAN_address() -> str:
+    return OCEANtoken().address
 
 @enforce_types
 def fundOCEANFromAbove(dst_address:str, amount_base:int):
-    _OCEAN_TOKEN.transfer(dst_address, amount_base, {'from':GOD_ACCOUNT})
+    OCEANtoken().transfer(dst_address, amount_base, {'from':GOD_ACCOUNT})
