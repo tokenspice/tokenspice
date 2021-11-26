ae5e0a6e5316e659ee52a8c8ff8a0c9f1dba7d0d
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-26 15:59:15 +0100

    Add compile.sh to streamline compilation into a single command. Update and simplify README

---
+++ compile.sh
@@ -0,0 +1,24 @@
+echo "compile.sh: Compile sol057/..."
+cd sol057
+echo """compiler:
+   solc:
+       version: 0.5.7""" > brownie-config.yaml
+brownie compile
+rm brownie-config.yaml
+cd ..
+
+echo ""
+
+echo "compile.sh: Compile sol080/..."
+cd sol080
+echo """compiler:
+   solc:
+       version: 0.8.0""" > brownie-config.yaml
+brownie compile
+rm brownie-config.yaml
+cd ..
+
+echo ""
+
+echo "compile.sh: Done"
+
