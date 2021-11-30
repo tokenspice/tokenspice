94519759338ae8d5dbeb56fc5ff9674d16f1fe98
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-30 08:12:12 +0100

    fix some codacy complaints in RouterAgent

--- agents/RouterAgent.py
+++ agents/RouterAgent.py
@@ -13,9 +13,18 @@ from util.constants import S_PER_MONTH
 @enforce_types
 class RouterAgent(AgentBase.AgentBaseNoEvm):
     def __init__(self, name: str, USD: float, OCEAN: float, receiving_agents: dict):
-        """receiving_agents -- [agent_n_name] : method_for_%_going_to_agent_n
-        The dict values are methods, not floats, so that the return value
-        can change over time. E.g. percent_burn changes.
+        """Constructor.
+
+        Args:
+            name: str -- agent name
+            USD: float -- initial USD
+            OCEAN: float -- initial OCEAN
+            receiving_agents: dict of {agent_n_name:str : method_for_percent_going_to_agent_n:method
+
+        Note:
+            The dict values are methods, not floats, so that the return value
+            can change over time. E.g. percent_burn changes.
+
         """
         super().__init__(name, USD, OCEAN)
         self._receiving_agents = receiving_agents
@@ -46,20 +55,19 @@ class RouterAgent(AgentBase.AgentBaseNoEvm):
             self._transferOCEAN(state.getAgent(name), computePercent() * OCEAN)
 
     def monthlyUSDreceived(self, state) -> float:
-        """Amount of USD received in the past month.
-        Assumes that it disburses USD as soon as it gets it."""
+        """USD received in the past month. Disburses immediately on receipt."""
         tick1 = self._tickOneMonthAgo(state)
         tick2 = state.tick
         return float(sum(self._USD_per_tick[tick1 : tick2 + 1]))
 
     def monthlyOCEANreceived(self, state) -> float:
-        """Amount of OCEAN received in the past month.
-        Assumes that it disburses OCEAN as soon as it gets it."""
+        """OCEAN received in past month. Disburses immediately on receipt."""
         tick1 = self._tickOneMonthAgo(state)
         tick2 = state.tick
         return float(sum(self._OCEAN_per_tick[tick1 : tick2 + 1]))
 
-    def _tickOneMonthAgo(self, state) -> int:
+    @staticmethod
+    def _tickOneMonthAgo(state) -> int:
         t2 = state.tick * state.ss.time_step
         t1 = t2 - S_PER_MONTH
         if t1 < 0:
