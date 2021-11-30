06c8c79b1020cf4498a4d1f4f8e5a30dd16109c3
trentmc <5305452+trentmc@users.noreply.github.com>
2021-11-30 09:11:48 +0100

    add openzeppelin 4.2.0 to installs

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 echo "compile.sh: Install 3rd party libs"
 brownie pm install OpenZeppelin/openzeppelin-contracts@2.1.1
 brownie pm install OpenZeppelin/openzeppelin-contracts@4.0.0
+brownie pm install OpenZeppelin/openzeppelin-contracts@4.2.0
 
 echo "compile.sh: Compile sol057/..."
 cd sol057
