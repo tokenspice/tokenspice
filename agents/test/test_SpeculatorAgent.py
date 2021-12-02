028a1bb32486c09d06cd45b933fd98202763e0d4
trentmc <5305452+trentmc@users.noreply.github.com>
2021-12-02 15:02:30 +0100

    in spirit of DRY, reuse code

--- agents/test/test_SpeculatorAgent.py
+++ agents/test/test_SpeculatorAgent.py
@@ -1,17 +1,18 @@
+#This module tests both SpeculatorAgent *and* StakerspeculatorAgent.py
+#If their macro behavior diverges, then flesh out test_StakerspeculatorAgent.py
 from enforce_typing import enforce_types
 import pytest
 
 from agents.PoolAgent import PoolAgent
 from agents.SpeculatorAgent import SpeculatorAgent
+from agents.StakerspeculatorAgent import StakerspeculatorAgent
 from engine.AgentDict import AgentDict
 from util.constants import S_PER_HOUR
-
 
 class MockSS:
     def __init__(self):
         # seconds per tick
         self.time_step: int = S_PER_HOUR
-
 
 class MockState:
     def __init__(self):
@@ -21,30 +22,32 @@ class MockState:
     def addAgent(self, agent):
         self.agents[agent.name] = agent
 
-
 @enforce_types
-def test_constructor1():
-    agent = SpeculatorAgent("agent1", 0.1, 0.2)
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_constructor1(_AgentClass):
+    agent = _AgentClass("agent1", 0.1, 0.2)
     assert agent.USD() == 0.1
     assert agent.OCEAN() == 0.2
     assert agent._s_between_speculates > 0
 
-
 @enforce_types
-def test_constructor2():
-    agent = SpeculatorAgent("agent1", 0.1, 0.2, 3)
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_constructor2(_AgentClass):
+    agent = _AgentClass("agent1", 0.1, 0.2, 3)
     assert agent._s_between_speculates == 3
 
-
 @enforce_types
-def test_doSpeculateAction(alice_info):
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_doSpeculateAction(alice_info, _AgentClass):
     alice_pool = alice_info.pool
     state = MockState()
 
-    agent = SpeculatorAgent("agent1", USD=0.0, OCEAN=0.0)
+    agent = _AgentClass(
+        "agent1", USD=0.0, OCEAN=0.0, s_between_speculates=1000
+    )
 
     assert agent._s_since_speculate == 0
-    assert agent._s_between_speculates > 0
+    assert agent._s_between_speculates == 1000
 
     assert not agent._doSpeculateAction(state)
 
@@ -57,42 +60,42 @@ def test_doSpeculateAction(alice_info):
 
     assert agent._doSpeculateAction(state)
 
-
 @enforce_types
-def test_speculateAction_nopools(alice_info):
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_speculateAction_nopools(alice_info, _AgentClass):
     alice_pool = alice_info.pool
     state = MockState()
 
-    agent = SpeculatorAgent("agent1", USD=0.0, OCEAN=1000.0)
+    agent = _AgentClass("agent1", USD=0.0, OCEAN=1000.0)
 
     assert not agent._poolsForSpeculate(state)
     assert not agent._doSpeculateAction(state)
     with pytest.raises(AssertionError):
         agent._speculateAction(state)  # error because no pools
 
-
 @enforce_types
-def test_speculateAction_with_rugged_pools(alice_info):
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_speculateAction_with_rugged_pools(alice_info, _AgentClass):
     alice_pool = alice_info.pool
     state = MockState()
     state.agents["pool1"] = PoolAgent("pool1", alice_pool)
     state.rugged_pools = ["pool1"]
 
-    agent = SpeculatorAgent("agent1", USD=0.0, OCEAN=500.0)
+    agent = _AgentClass("agent1", USD=0.0, OCEAN=500.0)
 
     assert not agent._poolsForSpeculate(state)
     assert not agent._doSpeculateAction(state)
     with pytest.raises(AssertionError):
         agent._speculateAction(state)  # error because no good pools
 
-
 @enforce_types
-def test_speculateAction_with_good_pools(alice_info):
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_speculateAction_with_good_pools(alice_info, _AgentClass):
     alice_pool = alice_info.pool
     state = MockState()
     state.agents["pool1"] = PoolAgent("pool1", alice_pool)
 
-    agent = SpeculatorAgent("agent1", USD=0.0, OCEAN=500.0)
+    agent = _AgentClass("agent1", USD=0.0, OCEAN=500.0)
 
     assert agent._poolsForSpeculate(state)
 
@@ -102,20 +105,26 @@ def test_speculateAction_with_good_pools(alice_info):
 
     dt = state.agents["pool1"].datatoken
     assert agent.OCEAN() == 500.0
-    assert agent.DT(dt) == 0.0
+    if _AgentClass == SpeculatorAgent:
+        assert agent.DT(dt) == 0.0
+    else:
+        assert agent.BPT(alice_pool) == 0.0
 
     agent._speculateAction(state)
     assert agent.OCEAN() < 500.0
-    assert agent.DT(dt) > 0.0
-
+    if _AgentClass == SpeculatorAgent:
+        assert agent.DT(dt) > 0.0
+    else:
+        assert agent.BPT(alice_pool) > 0.0
 
 @enforce_types
-def test_take_step(alice_info):
+@pytest.mark.parametrize("_AgentClass",[SpeculatorAgent,StakerspeculatorAgent])
+def test_take_step(alice_info, _AgentClass):
     alice_pool = alice_info.pool
     state = MockState()
     state.agents["pool1"] = PoolAgent("pool1", alice_pool)
 
-    agent = SpeculatorAgent("agent1", USD=0.0, OCEAN=500.0)
+    agent = _AgentClass("agent1", USD=0.0, OCEAN=500.0)
 
     num_speculates = num_loops = 0
     while num_speculates < 5 and num_loops < 10000:
