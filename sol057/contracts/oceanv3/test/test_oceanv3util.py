5a0f7560b792b0e654a7ce28c229feefda5a250a
Trent McConaghy <5305452+trentmc@users.noreply.github.com>
2021-12-02 10:23:11 +0100

    Document approach to unit testing oceanv3util.py
    Fix #116

---
+++ sol057/contracts/oceanv3/test/test_oceanv3util.py
@@ -0,0 +1,3 @@
+# oceanv2util.py is tested piecewise by other test files in this directory,
+# such as test_bfactory.py. Therefore we don't need to repeat ourselves and
+# have tests in this file. We keep this file, and these comments, as a reminder.
