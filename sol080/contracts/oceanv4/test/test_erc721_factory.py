0bdcfbc484737c03f517e11ebc6bea3f6c49fa13
nvtrang91 <nvtrang91@gmail.com>
2021-12-04 19:49:36 +0700

    test_erc721_factory_wip

--- sol080/contracts/oceanv4/test/test_erc721_factory.py
+++ sol080/contracts/oceanv4/test/test_erc721_factory.py
@@ -9,11 +9,14 @@ address0 = account0.address
 
 OPF_ADDRESS = OPF_ACCOUNT.address
 
+accounts = brownie.network.accounts
+
 def test_direct():
-    template_erc721= BROWNIE_PROJECT080.ERC721Template.deploy(
+    erc721_token= BROWNIE_PROJECT080.ERC721Template.deploy(
         {"from": account0}
     )
 
+    # Step 1: DataPublisher interacts with NFTFactory
     tt = BROWNIE_PROJECT080.V3DataTokenTemplate.deploy(
         "TT",
         "TemplateToken",
@@ -27,24 +30,23 @@ def test_direct():
     dtfactory = BROWNIE_PROJECT080.V3DTFactory.deploy(
         tt.address, account0.address, {"from": account0}
     )
-    tx1 = dtfactory.createToken(
+    tx2 = dtfactory.createToken(
         "foo_blob", "DT", "DT", toBase18(100.0), {"from": account0}
     )
-    dt_address = tx1.events["TokenCreated"]["newTokenAddress"]
+    dt_address = tx2.events["TokenCreated"]["newTokenAddress"]
 
+    # Deploy ERC721Factory
     erc721_factory = BROWNIE_PROJECT080.ERC721Factory.deploy(
-        template_erc721.address,
+        erc721_token.address,
         dt_address,
         OPF_ADDRESS,
         address0,
         {"from":account0}
     )
 
-    # registered_event = brownie.chain.get_transaction(erc721_factory.tx.txid).events
-    # assert registered_event["OwnershipTransferred"]["newOwner"] == account0
-    # assert registered_event["Template721Added"]["_templateAddress"] == template_erc721.address
-
     current_nft_count = erc721_factory.getCurrentNFTCount()
+
+    # Deploy ERC721Contract
     tx2 = erc721_factory.deployERC721Contract(
         "NFT",
         "NFTSYMBOL",
@@ -63,16 +65,25 @@ def test_direct():
 
     # Tests get NFT template
     nft_template = erc721_factory.getNFTTemplate(1)
-    assert nft_template[0] == template_erc721.address
+    assert nft_template[0] == erc721_token.address
     assert nft_template[1] is True
 
-    # Tests creating successfully an ERC20 token
-    erc721_factory.createToken(
-        1, 
-        ["DT","sym"], 
-        [address0,address0,address0,address0],
-        [1e3,0.1],
-        [1],
-        {"from": account0}
+    # Step 2: Publisher createERC20
+    erc721_token.initialize(
+        address0,
+        "NFT1",
+        "NFTSYMBOL1",
+        erc721_token.address,
+        address0,
+        "https://oceanprotocol.com/nft/",
+        {"from":account0}
     )
 
+    # Tests creating successfully an ERC20 token
+    erc721_token.createERC20(1,
+        ["ERC20DT1", "ERC20DT1Symbol"], 
+        [address0, accounts[1].address, accounts[2].address, accounts[3].address],
+        [100,0],
+        [b""], 
+        {"from":account0}
+    )
