368c25189a5edec8ae8c34b5e807aa33f4a770fa
trentmc <5305452+trentmc@users.noreply.github.com>
2021-12-05 09:00:57 +0100

    can now build erc721 factory, deploy erc721, deploy erc20

--- sol080/contracts/oceanv4/test/test_erc721_factory.py
+++ sol080/contracts/oceanv4/test/test_erc721_factory.py
@@ -13,7 +13,7 @@ ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 accounts = brownie.network.accounts
 
 def test_trent_play():
-    #deploy factory
+    #deploy templates, then factory
     erc721_template = BROWNIE_PROJECT080.ERC721Template.deploy(
         {"from": account0}
     )
@@ -30,17 +30,41 @@ def test_trent_play():
         {"from":account0}
     )
 
-    #from factory, deploy a data NFT
-    template_index = 1 #refer to erc721_template
+    #from factory, deploy an ERC721 data NFT
+    erc721_template_index = 1 #refer to erc721_template
     additional_NFT_deployer_address = ZERO_ADDRESS
     token_URI = "https://mystorage.com/mytoken.png"
-    erc721_factory.deployERC721Contract(
+    tx = erc721_factory.deployERC721Contract(
         "data NFT 1",
         "DATANFT1",
-        template_index,
+        erc721_template_index,
         additional_NFT_deployer_address,
-        token_URI)
+        token_URI,
+        {"from":account0}
+    )
+    dataNFT1_address = tx.events['NFTCreated']['newTokenAddress']
+    dataNFT1 = BROWNIE_PROJECT080.ERC721Template.at(dataNFT1_address)
 
+    #from erc271 data NFT, deploy an ERC20 datatoken
+    erc20_template_index = 1 #refer to erc20_template
+    strings = ["datatoken 1", "DT1"] #name, symbol
+    addresses = [address0]*4 #minter, fee mgr, pub mkt addr, pub mkt fee token
+    uints = [toBase18(1000.0), toBase18(0.1)] # erc20 cap, pub mkt fee amt
+    bytes = []
+    tx = dataNFT1.createERC20(
+        erc20_template_index,
+        strings,
+        addresses,
+        uints,
+        bytes,
+        {"from":account0}
+    )
+    datatoken1_address = tx.events['TokenCreated']['newTokenAddress']
+    datatoken1 = BROWNIE_PROJECT080.ERC20Template.at(1_address)
+    #FIXME: solve warning "Event log does not contain enough topics for the given ABI - this is usually because an event argument is not marked as indexed"
+
+    #from ?, deploy bpool
+    #FIXME
     
 def test_direct():
     erc721_token= BROWNIE_PROJECT080.ERC721Template.deploy(
