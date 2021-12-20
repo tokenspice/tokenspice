c646893eded244182904ff87218eb6c5c6231f72
nvtrang91 <nvtrang91@gmail.com>
2021-12-20 09:45:34 +0700

    modify test_erc721_factory

--- sol080/contracts/oceanv4/test/test_erc721_factory.py
+++ sol080/contracts/oceanv4/test/test_erc721_factory.py
@@ -1,6 +1,6 @@
 import brownie
 from sol080.contracts.oceanv4.oceanv4util import createDataNFT, create_datatoken_from_dataNFT, \
-    create_BPool_from_datatoken,poolAddressFromNewBPoolTx, ROUTER
+    create_BPool_from_datatoken,poolAddressFromNewBPoolTx, ROUTER, OCEANtoken
 from util.base18 import toBase18
 from util.constants import BROWNIE_PROJECT080, OPF_ACCOUNT, GOD_ACCOUNT
 
@@ -42,8 +42,7 @@ def test_direct():
         {"from":account0}
     )
 
-    assert erc721_factory.tx.events["OwnershipTransferred"]["newOwner"] == address0
-    assert erc721_factory.tx.events["Template20Added"]["_templateAddress"] == erc20_template.address
+    assert erc721_factory.owner() == address0
 
     current_nft_count = erc721_factory.getCurrentNFTCount()
 
@@ -52,7 +51,7 @@ def test_direct():
     additional_NFT_deployer_address = ZERO_ADDRESS
     token_URI = "https://mystorage.com/mytoken.png"
     tx = erc721_factory.deployERC721Contract(
-        "data NFT 1",
+        "dataNFT1",
         "DATANFT1",
         erc721_template_index,
         additional_NFT_deployer_address,
@@ -60,13 +59,14 @@ def test_direct():
         {"from":account0}
     )
 
+    assert tx.events["NFTCreated"] is not None
     assert tx.events["NFTCreated"]["admin"] == address0
     
     # ERC721 token
     dataNFT1_address = tx.events["NFTCreated"]["newTokenAddress"]
     dataNFT1 = BROWNIE_PROJECT080.ERC721Template.at(dataNFT1_address)
     
-    assert dataNFT1.name() == "data NFT 1"
+    assert dataNFT1.name() == "dataNFT1"
     assert dataNFT1.symbol() == "DATANFT1"
 
     # Tests current NFT count
@@ -79,7 +79,7 @@ def test_direct():
 
     #2: Publisher createERC20
     erc20_template_index = 1 #refer to erc20_template
-    DT_name, DT_symbol = "datatoken 1", "DT1"
+    DT_name, DT_symbol = "datatoken1", "DT1"
     strings = [DT_name, DT_symbol]
     minter_addr = fee_mgr_addr = pub_mkt_addr = address0
     pub_mkt_fee_token_addr = ZERO_ADDRESS
@@ -100,13 +100,24 @@ def test_direct():
     DT_address = tx.events['TokenCreated']['newTokenAddress']
     DT = BROWNIE_PROJECT080.ERC20Template.at(DT_address)
 
+    assert DT.name() == DT_name
+    assert DT.symbol() == DT_symbol
+    assert DT.cap() == toBase18(DT_cap)
+
     # Tests ERC20 token template list
     assert erc721_factory.getTokenTemplate(1)[0] == erc20_template.address
     assert erc721_factory.getTokenTemplate(1)[1] == True
 
     # Tests balanceOf
-    assert DT.balanceOf(minter_addr) == 0
+    assert DT.balanceOf(address0) == 0
 
+    # Once ERC20 DT has been created, publisher has 3 options:
+    # Details here: https://github.com/oceanprotocol/contracts/blob/v4main_postaudit/docs/quickstart_pubFlow.md
+    # 1. Create pool
+    # 2. Mint DTs and create fixed price exchange
+    # 3. Mint DTs and created pools on any other market
+
+    # FOCUS on option 1
     #3 Publisher creating bpool, from ERC20 datatoken
     ss_rate = 0.1
     ss_OCEAN_decimals = 18
@@ -114,8 +125,8 @@ def test_direct():
     ss_DT_vested_blocks = 2500000 # = num blocks/year, if 15 s/block
     ss_OCEAN_init_liquidity = 2000.0
 
-    LP_swap_fee = 0.001 # 1%
-    mkt_swap_fee = 0.001 # 1%
+    LP_swap_fee = 0.02 # 2%
+    mkt_swap_fee = 0.01 # 1%
     
     sideStaking = BROWNIE_PROJECT080.SideStaking.deploy(
         router.address, {'from': GOD_ACCOUNT}
@@ -145,21 +156,38 @@ def test_direct():
     )
     pool_address = poolAddressFromNewBPoolTx(tx)
     pool = BROWNIE_PROJECT080.BPool.at(pool_address)
-    #FIXME: assertions
-    # Check initial ocean tokens at the beginning
-    OCEANtoken.balanceOf(pool_address) == toBase18(ss_OCEAN_init_liquidity)
+
+    assert OCEANtoken.balanceOf(pool_address) == toBase18(ss_OCEAN_init_liquidity)
+    assert pool.getMarketFee() == toBase18(mkt_swap_fee)
+    assert pool.getSwapFee() == toBase18(LP_swap_fee)
 
 
 def test_createDataNFT_via_util():
     dataNFT = createDataNFT("dataNFT", "DATANFT", account0)
+    assert dataNFT.name() == "dataNFT"
+    assert dataNFT.symbol() == "DATANFT"
+    assert dataNFT.getPermissions(account0.address) == (True, True, True, True)
+    assert dataNFT.balanceOf(account0.address) == 1
+
 
 def test_createDT_via_util():
     dataNFT = createDataNFT("dataNFT", "DATANFT", account0)
     DT = create_datatoken_from_dataNFT("DT", "DTSymbol", 10000, dataNFT, account0)
+    assert DT.name() == "DT"
+    assert DT.symbol() == "DTSymbol"
+    assert DT.cap() == toBase18(10000)
+    assert DT.balanceOf(account0.address) == 0
 
 def test_createBPool_via_util():
     router = ROUTER()
-    router.changeRouterOwner(account0.address, {"from":GOD_ACCOUNT})
     dataNFT = createDataNFT("dataNFT", "DATANFT", account0, router)
     DT = create_datatoken_from_dataNFT("DT", "DTSymbol", 10000, dataNFT, account0)
-    pool = create_BPool_from_datatoken(DT, 10, 200, account0)
+    pool = create_BPool_from_datatoken(DT, 10, 2000, account0)
+    pool_address = pool.address
+
+    OCEAN = OCEANtoken()
+    assert pool.getBaseTokenAddress() == OCEAN.address
+
+    assert OCEAN.balanceOf(pool_address) == toBase18(2000)
+    assert pool.getMarketFee() == toBase18(0.01)
+    assert pool.getSwapFee() == toBase18(0.02)
