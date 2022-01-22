d7db395dbf5629c339723fa87246b6c7ce4df28e
trentmc <5305452+trentmc@users.noreply.github.com>
2022-01-22 11:08:37 +0100

    Fix #138 - separate Brownie package installs from compiles, and make the former more explicit in README

--- README.md
+++ README.md
@@ -71,6 +71,9 @@ source venv/bin/activate
 
 #install dependencies
 pip install -r requirements.txt
+
+#install brownie packages (you can ignore FileExistsErrors)
+./brownie-install.sh
 ```
 
 ## TokenSPICE CLI
---
+++ brownie-install.sh
@@ -0,0 +1,8 @@
+echo "brownie-install.sh: Install 3rd party libs"
+brownie pm install OpenZeppelin/openzeppelin-contracts@2.1.1
+brownie pm install OpenZeppelin/openzeppelin-contracts@4.0.0
+brownie pm install OpenZeppelin/openzeppelin-contracts@4.2.0
+brownie pm install GNSPS/solidity-bytes-utils@0.8.0
+
+echo "brownie-install.sh: Done"
+
