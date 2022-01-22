702848c0c9c0a10ab71dc1525b79ede140c12eb4
trentmc <5305452+trentmc@users.noreply.github.com>
2022-01-22 09:21:31 +0100

    Fix #130, by adding another 7 accounts to ganache.py

--- ganache.py
+++ ganache.py
@@ -34,6 +34,16 @@ amount_wei = eth_utils.to_wei(amount_eth, "ether")
 
 ganache_command = f'ganache-cli --port {port_number} --gasLimit 10000000000 --gasPrice 1 ---hardfork istanbul --account="{alice_private_key},{amount_wei}" --account="{bob_private_key},{amount_wei}" --account="{factory_deployer_private_key},{amount_wei}"'  # pylint: disable=line-too-long
 
+#add 7 more accounts, for 10 total. Private keys were chosen arbitrarily.
+for private_key in [
+        "0x69a0315f0a6932ca52d5b1ad9ce31b2fef7de658f8da625a6d97f4dbb3ba22c1",
+        "0x2c06b48c205efccc3506430212630a11bcc99cad1994452898e5df63985eda10",
+        "0xd890fedd404c6f49daf4be91cd720df22786e1d35d579b8372cc531eed80a267",
+        "0x9da43e9603043299cd6c5aecec69b7713342496f3465caaadbee5db955f18010",
+        "0x53a2a4124387132ceae955edb80f13aa549f2e956d3f8aae0383412a3c765a93",
+        "0x1803ea57835da6f03f8b43458482f65f280600c278947fe0eaf78c5d7d260c81",
+        "0xb13f2706716d269a9639f2eb99d38ba8aaef0e210d1b35a2e40e3e8b62ab76f9"]:
+    ganache_command += f' --account="{private_key}"'
 
 if args.run_in_background:
     os.system(ganache_command + "&")
